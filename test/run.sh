359b3b606d207b5a2e89fb39694bc2c0ada4cc89
Rob Kellett <rob@kelle.tt>
2014-07-15 00:23:48 -0400

    Test script: fifth time's the charm

--- test/run.sh
+++ test/run.sh
@@ -5,16 +5,15 @@
 # bail on error
 set -e
 
-# Download & install content_shell if not found
-which content_shell
-if [[ $? -ne 0 ]]; then
-  $DART_SDK/../chromium/download_contentshell.sh
-  unzip content_shell-linux-x64-release.zip
+# Download & install content_shell
+echo Downloading content_shell...
+$DART_SDK/../chromium/download_contentshell.sh
+unzip content_shell-linux-x64-release.zip
 
-  cs_path=$(ls -d drt-*)
-  PATH=$cs_path:$PATH
-fi
+cs_path=$(ls -d drt-*)
+PATH=$cs_path:$PATH
 
+echo Running tests...
 DUMP=$(content_shell --dump-render-tree test/test.html)
 echo "$DUMP"
 
