2d01c95bfa421e92763da3246cf4cc99d0ae4184
Rob Kellett <rob@kelle.tt>
2014-07-14 23:48:27 -0400

    Script for automated unit testing

---
+++ test/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Thanks to Kevin Moore (http://work.j832.com/) for this script
+
+# bail on error
+set -e
+
+DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
+
+BROWSER_TEST_FILE=$DIR/../test/test.html
+
+echo DumpRenderTree must be in your path for this to work
+echo Running against test file at:
+echo $BROWSER_TEST_FILE
+
+DUMP=$(DumpRenderTree $BROWSER_TEST_FILE)
+echo "$DUMP"
+
+REGEX="All [0-9]+ tests pass"
+
+if [[ $DUMP =~ $REGEX ]]
+then
+  echo Success!
+  exit 0
+else
+  echo Fail!
+  exit 1
+fi
